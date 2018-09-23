169d464a88e6e979f09d23de006003f8d16ad562
def <def@huumeet.info>
2018-09-23 10:19:33 +0300

    Initial commit (alpha version)

---
+++ README.md
@@ -0,0 +1,5 @@
+Basic functionality is there. Some refactoring needed, stay tuned ...
+
+TODO: Write README you lazy fuck
+
+Compile: `fasm hotfinger.asm`
---
+++ hotfinger.asm
@@ -0,0 +1,521 @@
+format PE GUI 4.0
+
+include 'win32a.inc'
+include 'winbio32equ.inc'
+
+;-------------------------------------------------------------------------
+; Program settings
+;-------------------------------------------------------------------------
+struct GUI_SETTINGS
+  x  dd ?
+  y  dd ?
+  cx dd ?
+  cy dd ?
+ends
+
+struct COMMAND_SETTINGS
+  RThumb  db MAX_PATH dup (?)
+  RIndex  db MAX_PATH dup (?)
+  RMiddle db MAX_PATH dup (?)
+  RRing   db MAX_PATH dup (?)
+  RLittle db MAX_PATH dup (?)
+  LThumb  db MAX_PATH dup (?)
+  LIndex  db MAX_PATH dup (?)
+  LMiddle db MAX_PATH dup (?)
+  LRing   db MAX_PATH dup (?)
+  LLittle db MAX_PATH dup (?)
+ends
+
+struct DEVICE_SETTINGS
+  wszDeviceInstanceId du WINBIO_MAX_STRING_LEN dup (?)
+ends
+
+struct VOLATILE_SETTINGS
+  dwUnitId dd ? ; WINBIO_UNIT_SCHEMA
+ends
+
+struct SETTINGS
+  GUI GUI_SETTINGS
+  dev DEVICE_SETTINGS
+  cmd COMMAND_SETTINGS
+  vol VOLATILE_SETTINGS
+ends
+
+section '.text' code readable executable
+entry $
+                push SW_SHOWDEFAULT
+                call [GetCommandLineA]
+                push eax
+                push NULL
+                push NULL
+                call [GetModuleHandleA]
+                push eax
+                call WinMain
+
+                push eax
+                call [ExitProcess]
+
+;-------------------------------------------------------------------------;
+; WinMain
+;-------------------------------------------------------------------------;
+proc WinMain hInst:DWORD, hPrevInst:DWORD, szCmdLine:DWORD, nCmdShow:DWORD
+                local ini:SETTINGS
+                local wc:WNDCLASSEX
+                local msg:MSG
+                local icl:INITCOMMONCONTROLSEX
+                local pUnits:DWORD
+                local dwCount:DWORD
+                local rc:DWORD
+
+                push ebx
+                push esi
+                push edi
+
+                xor eax, eax
+                mov [pUnits], eax
+                mov [dwCount], eax
+                inc eax
+                mov [rc], eax
+
+                ; Initialize common controls & OLE
+                push sizeof.INITCOMMONCONTROLSEX
+                push ICC_LISTVIEW_CLASSES or ICC_BAR_CLASSES
+                lea eax, [icl]
+                pop [icl.dwICC]
+                pop [icl.dwSize]
+                push eax
+                call [InitCommonControlsEx]
+                push NULL
+                call [CoInitialize]
+
+                ; Parse command-line arguments
+                call CommandLineInterface
+
+                ; Read settings
+                lea ebx, [ini]
+                push ebx
+                call ReadAppDataSettings
+
+                ; Get sensor (validate the one read from settings)
+                ; TODO: Move this to settings.inc or somewhere else
+.retry:         lea eax, [dwCount]
+                push eax
+                lea ecx, [pUnits]
+                push ecx
+                push WINBIO_TYPE_FINGERPRINT
+                call [WinBioEnumBiometricUnits]
+                cmp eax, RPC_S_SERVER_TOO_BUSY
+                jz .retry
+                test eax, eax
+                jz .enumerated
+
+                mov eax, hotfinger.szSensorUnavailable
+                lea ecx, [eax + hotfinger.szEnumSensorUnavailable - hotfinger.szSensorUnavailable]
+                push MB_ICONEXCLAMATION+MB_OK
+                push eax
+                push ecx
+                push NULL
+                call [MessageBoxA]
+                jmp .die
+
+.enumerated:    lea ecx, [eax-1]
+                lea esi, [ebx + SETTINGS.dev.wszDeviceInstanceId]
+                mov edi, esi
+                repnz scasw
+                xor ecx, -1
+
+                mov eax, [pUnits]
+                xor edx, edx
+.sensor_loop:   cmp edx, [dwCount]
+                jae .select_sensor
+
+                lea edi, [eax + WINBIO_UNIT_SCHEMA.wszDeviceInstanceId]
+                push ecx
+                push esi
+                rep cmpsw
+                pop esi
+                pop ecx
+                jnz .next_sensor
+
+                call .check_sensor
+                jnz .found_sensor
+
+.next_sensor:   add eax, sizeof.WINBIO_UNIT_SCHEMA
+                inc edx
+                jmp .sensor_loop
+
+                ; Input = wszDeviceInstanceId (esi)
+.check_sensor:  push eax
+                push edx
+                push guidDatabase
+                push esi
+                call FindPrivateSensorKeyForDatabase
+                test eax, eax
+                jz @f
+                pushf
+                push eax
+                call [RegCloseKey]
+                popf
+@@:             pop edx
+                pop eax
+                retn
+
+.select_sensor: push [dwCount]
+                push [pUnits]
+                push guidDatabase
+                call SensorSelect
+                test eax, eax
+                jz @f
+                lea esi, [eax + WINBIO_UNIT_SCHEMA.wszDeviceInstanceId]
+                call .check_sensor
+                jnz .found_sensor
+@@:             push [pUnits]
+                call [WinBioFree]
+                jmp .die
+
+.found_sensor:  push [eax + WINBIO_UNIT_SCHEMA.dwUnitId]
+                pop [ebx + SETTINGS.vol.dwUnitId]
+                mov ecx, WINBIO_MAX_STRING_LEN
+                lea esi, [eax + WINBIO_UNIT_SCHEMA.wszDeviceInstanceId]
+                lea edi, [ebx + SETTINGS.dev.wszDeviceInstanceId]
+
+@@:             lodsw
+                test ax, ax
+                jz @f
+                stosw
+                loop @b
+@@:             rep stosw
+                push [pUnits]
+                call [WinBioFree]
+
+;--------------------------------------------------------------------------;
+; Register window class
+;--------------------------------------------------------------------------;
+                xor eax, eax
+                lea ecx, [eax + sizeof.WNDCLASSEX]
+                push ecx
+                lea edi, [wc]
+                rep stosb
+                pop [wc.cbSize]
+                mov [wc.style], CS_HREDRAW or CS_VREDRAW or CS_BYTEALIGNWINDOW
+                mov [wc.lpfnWndProc], WndProc
+                push [hInst]
+                pop [wc.hInstance]
+
+                push IDI_MAIN
+                push [hInst]
+                call [LoadIconA]
+                mov [wc.hIcon], eax
+                mov [wc.hIconSm], eax
+
+                push IDC_ARROW
+                push NULL
+                call [LoadCursorA]
+                mov [wc.hCursor], eax
+
+                mov [wc.hbrBackground], COLOR_BTNFACE+1
+                mov [wc.lpszClassName], gui.szClassName
+
+                lea eax, [wc]
+                push eax
+                call [RegisterClassExA]
+                test eax, eax
+                jnz .create_window
+
+                push MB_ICONERROR
+                push gui.szError
+                push gui.szWindowRegistrationFailed
+                push NULL
+                call [MessageBoxA]
+                jmp .die
+
+;--------------------------------------------------------------------------;
+; Create the window
+;--------------------------------------------------------------------------;
+.create_window: push ebx
+                push [hInst]
+                push NULL
+                push NULL
+
+                push [ini + SETTINGS.GUI.cy] ; height
+                mov ecx, [ini + SETTINGS.GUI.y]
+                sub [esp], ecx
+                push [ini + SETTINGS.GUI.cx] ; width
+                mov eax, [ini + SETTINGS.GUI.x]
+                sub [esp], eax
+
+                push ecx ; y
+                push eax ; x
+
+                push WS_OVERLAPPEDWINDOW
+                push gui.szCaption
+                push gui.szClassName
+                push 0
+                call [CreateWindowExA]
+                test eax, eax
+                jnz .show_window
+
+                push MB_ICONERROR
+                push gui.szError
+                push gui.szWindowCreationFailed
+                push NULL
+                call [MessageBoxA]
+                jmp .die
+
+.show_window:   mov esi, eax
+                lea edi, [msg]
+                push esi
+                push [nCmdShow]
+                push esi
+                call [ShowWindow]
+                call [UpdateWindow]
+
+;--------------------------------------------------------------------------;
+; Enter message loop
+;--------------------------------------------------------------------------;
+.msg_loop:      push 0
+                push 0
+                push NULL
+                push edi
+                call [GetMessageA]
+                test eax, eax
+                jz .finish
+
+                push edi
+                push esi
+                call [IsDialogMessage]
+                test eax, eax
+                jnz .msg_loop
+
+                push edi
+                push edi
+                call [TranslateMessage]
+                call [DispatchMessageA]
+                jmp .msg_loop
+.finish:        push [msg.wParam]
+                pop [rc]
+
+;--------------------------------------------------------------------------;
+; Finish
+;--------------------------------------------------------------------------;
+                cmp word [ini.dev.wszDeviceInstanceId], 0
+                jz .die ; uninstalled or something, do not save settings
+
+                push ebx
+                call WriteAppDataSettings
+
+.die:           call [CoUninitialize]
+
+                pop edi
+                pop esi
+                pop ebx
+                mov eax, [rc]
+                ret
+endp
+
+include 'cli.inc'
+include 'database.inc'
+include 'gui.inc'
+include 'sensor.inc'
+include 'settings.inc'
+include 'utils.inc'
+
+section '.data' data readable writeable
+include 'data.inc'
+
+section '.idata' import readable
+  library advapi32, 'advapi32.dll', \
+          comctl32, 'comctl32.dll', \
+          comdlg32, 'comdlg32.dll', \
+          gdi32, 'gdi32.dll', \
+          kernel32, 'kernel32.dll', \
+          ole32, 'ole32.dll', \
+          shell32, 'shell32.dll', \
+          shlwapi, 'shlwapi.dll', \
+          user32, 'user32.dll', \
+          winbio, 'winbio.dll'
+
+  include 'winbio32api.inc'
+
+  import advapi32, \
+         RegCloseKey, 'RegCloseKey', \
+         RegCreateKeyExA, 'RegCreateKeyExA', \
+         RegDeleteKeyA, 'RegDeleteKeyA', \
+         RegEnumKeyExA, 'RegEnumKeyExA', \
+         RegGetValueA, 'RegGetValueA', \
+         RegOpenKeyExA, 'RegOpenKeyExA', \
+         RegSetValueExA, 'RegSetValueExA'
+
+  import comctl32, \
+         DefSubclassProc, 'DefSubclassProc', \
+         InitCommonControlsEx, 'InitCommonControlsEx', \
+         RemoveWindowSubclass, 'RemoveWindowSubclass', \
+         SetWindowSubclass, 'SetWindowSubclass'
+
+  import comdlg32, \
+         GetOpenFileNameA, 'GetOpenFileNameA'
+
+  import gdi32, \
+         GetStockObject, 'GetStockObject'
+
+  import kernel32, \
+         CloseHandle, 'CloseHandle', \
+         CreateDirectoryA, 'CreateDirectoryA', \
+         DeleteFileA, 'DeleteFileA', \
+         ExitProcess, 'ExitProcess', \
+         GetCommandLineA, 'GetCommandLineA', \
+         GetCommandLineW, 'GetCommandLineW', \
+         GetExitCodeProcess, 'GetExitCodeProcess', \
+         GetFileAttributesA, 'GetFileAttributesA', \
+         GetLastError, 'GetLastError', \
+         GetModuleFileNameW, 'GetModuleFileNameW', \
+         GetModuleHandleA, 'GetModuleHandleA', \
+         GetPrivateProfileIntA, 'GetPrivateProfileIntA', \
+         GetPrivateProfileStringA, 'GetPrivateProfileStringA', \
+         GetProcessHeap, 'GetProcessHeap', \
+         HeapAlloc, 'HeapAlloc', \
+         HeapFree, 'HeapFree', \
+         HeapReAlloc, 'HeapReAlloc', \
+         LocalFree, 'LocalFree', \
+         MulDiv, 'MulDiv', \
+         RemoveDirectoryA, 'RemoveDirectoryA', \
+         WaitForSingleObject, 'WaitForSingleObject', \
+         WritePrivateProfileStringA, 'WritePrivateProfileStringA', \
+         lstrcmpW, 'lstrcmpW', \
+         lstrlenA, 'lstrlenA', \
+         lstrlenW, 'lstrlenW'
+
+  import ole32, \
+         CoInitialize, 'CoInitialize', \
+         CoInitializeEx, 'CoInitializeEx', \
+         CoUninitialize, 'CoUninitialize'
+
+  import shell32, \
+         CommandLineToArgvW, 'CommandLineToArgvW', \
+         SHGetFileInfo, 'SHGetFileInfo', \
+         SHGetFolderPathA, 'SHGetFolderPathA', \
+         ShellExecuteA, 'ShellExecuteA', \
+         ShellExecuteExW, 'ShellExecuteExW', \
+         ShellExecuteW, 'ShellExecuteW'
+
+  import shlwapi, \
+         SHAutoComplete, 'SHAutoComplete'
+
+  import user32, \
+        AppendMenuA, 'AppendMenuA', \
+        CreateMenu, 'CreateMenu', \
+        CreatePopupMenu, 'CreatePopupMenu', \
+        CreateWindowExA, 'CreateWindowExA', \
+        DefWindowProcA, 'DefWindowProcA', \
+        DestroyWindow, 'DestroyWindow', \
+        DialogBoxParamA, 'DialogBoxParamA', \
+        DispatchMessageA, 'DispatchMessageA', \
+        EnableWindow, 'EnableWindow', \
+        EndDialog, 'EndDialog', \
+        GetClientRect, 'GetClientRect', \
+        GetDlgItem, 'GetDlgItem', \
+        GetDpiForSystem, 'GetDpiForSystem', \
+        GetDpiForWindow, 'GetDpiForWindow', \
+        GetMessageA, 'GetMessageA', \
+        GetSystemMetricsForDpi, 'GetSystemMetricsForDpi', \
+        GetWindowLongA, 'GetWindowLongA', \
+        GetWindowRect, 'GetWindowRect', \
+        GetWindowTextA, 'GetWindowTextA', \
+        GetWindowTextLengthA, 'GetWindowTextLengthA', \
+        IsDialogMessage, 'IsDialogMessage', \
+        LoadCursorA, 'LoadCursorA', \
+        LoadIconA, 'LoadIconA', \
+        MessageBoxA, 'MessageBoxA', \
+        MoveWindow, 'MoveWindow', \
+        PostQuitMessage, 'PostQuitMessage', \
+        RedrawWindow, 'RedrawWindow', \
+        RegisterClassExA, 'RegisterClassExA', \
+        SendDlgItemMessageA, 'SendDlgItemMessageA', \
+        SendMessageA, 'SendMessageA', \
+        SetFocus, 'SetFocus', \
+        SetMenu, 'SetMenu', \
+        SetWindowLongA, 'SetWindowLongA', \
+        SetWindowTextA, 'SetWindowTextA', \
+        ShowWindow, 'ShowWindow', \
+        TranslateMessage, 'TranslateMessage', \
+        UpdateWindow, 'UpdateWindow', \
+        wsprintfA, 'wsprintfA'
+
+section '.rsrc' resource data readable
+
+; Dialogs
+IDD_SENSOR      = 100
+IDD_ABOUT       = 101
+
+; Icons
+IDI_MAIN        = 401
+
+; Sensor dialog
+IDC_BTN_SELECT     = 2000
+IDC_BTN_CANCEL     = 2001
+IDC_LIST_SENSORS   = 2002
+IDC_STATIC_MESSAGE = 2003
+
+; About dialog
+IDC_STATIC_ICON = 3000
+
+  directory RT_DIALOG, dialogs, \
+            RT_ICON, icons, \
+            RT_GROUP_ICON, group_icons, \
+            RT_MANIFEST, manifests, \
+            RT_VERSION, versions
+
+  resource dialogs, \
+           IDD_SENSOR, LANG_ENGLISH+SUBLANG_DEFAULT, sensor_dialog, \
+           IDD_ABOUT, LANG_ENGLISH+SUBLANG_DEFAULT, about_dialog
+  resource manifests, 1, LANG_NEUTRAL, manifest
+  resource versions,  1, LANG_NEUTRAL, version
+  resource icons,     1, LANG_NEUTRAL, icon_data
+  resource group_icons, IDI_MAIN, LANG_NEUTRAL, main_icon
+
+  DLG_WIDTH = 350
+  DLG_HEIGHT = 120
+  dialog sensor_dialog, 'Sensor dialog', 0,0,DLG_WIDTH,DLG_HEIGHT, WS_CAPTION+WS_POPUP+WS_SYSMENU+DS_MODALFRAME+DS_CENTER
+    dialogitem 'BUTTON','&Select', IDC_BTN_SELECT, DLG_WIDTH-108,DLG_HEIGHT-18,50,14, BS_DEFPUSHBUTTON+WS_VISIBLE+WS_TABSTOP+WS_DISABLED
+    dialogitem 'BUTTON','&Cancel', IDC_BTN_CANCEL, DLG_WIDTH-54,DLG_HEIGHT-18,50,14, BS_PUSHBUTTON+WS_VISIBLE+WS_TABSTOP
+    dialogitem 'SysListView32','', IDC_LIST_SENSORS, 4,4,DLG_WIDTH-8,DLG_HEIGHT-24, WS_VISIBLE+WS_BORDER+WS_TABSTOP+LVS_REPORT+LVS_SINGLESEL+LVS_SHOWSELALWAYS
+    dialogitem 'STATIC','Select sensor from the list', IDC_STATIC_MESSAGE, 4,DLG_HEIGHT-14,DLG_WIDTH-112,8, WS_VISIBLE
+  enddialog
+
+  dialog about_dialog,'About', 40,40,170,60, WS_CAPTION+WS_POPUP+WS_SYSMENU+DS_MODALFRAME
+    dialogitem 'STATIC', <'HotFinger application launcher',0dh,0ah,'https://github.com/resilar/HotFinger'>, -1, 24,10,146,40,WS_VISIBLE+SS_CENTER
+    dialogitem 'STATIC', IDI_MAIN, -1, 8,8,32,32, WS_VISIBLE+SS_ICON
+    dialogitem 'STATIC', '', -1, 4,35,162,11, WS_VISIBLE+SS_ETCHEDHORZ
+    dialogitem 'STATIC', <'HotFinger v0.0.0'>, -1, 4,44,100,22, WS_VISIBLE+SS_LEFT
+    dialogitem 'BUTTON', 'OK', IDOK, 124,40,42,16, WS_VISIBLE+WS_TABSTOP+BS_DEFPUSHBUTTON
+  enddialog
+
+  icon main_icon, icon_data, 'sysclass.ico'
+
+  resdata manifest
+    db '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',0dh,0ah
+    db '<assembly xmlns="urn:schemas-microsoft-com:asm.v1" xmlns:asmv3="urn:schemas-microsoft-com:asm.v3" manifestVersion="1.0">',0dh,0ah
+    db ' <assemblyIdentity version="1.0.0.0" processorArchitecture="x86" type="win32" name="hotfinger.exe"/>',0dh,0ah
+    db ' <description>HotFinger</description>',0dh,0ah
+    db ' <dependency>',0dh,0ah
+    db '  <dependentAssembly>',0dh,0ah
+    db '    <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls" version="6.0.0.0" processorArchitecture="x86" publicKeyToken="6595b64144ccf1df" language="*"/> ',0dh,0ah
+    db '  </dependentAssembly>',0dh,0ah
+    db ' </dependency>',0dh,0ah
+    db ' <asmv3:application>',0dh,0ah
+    db '  <asmv3:windowsSettings xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">',0dh,0ah
+    ;db '   <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>',0dh,0ah
+    db '   <dpiAware>true</dpiAware>',0dh,0ah
+    db '  </asmv3:windowsSettings>',0dh,0ah
+    db ' </asmv3:application>',0dh,0ah
+    db '</assembly>',0dh,0ah
+  endres
+
+  versioninfo version, \
+    VOS__WINDOWS32, VFT_APP,VFT2_UNKNOWN, LANG_ENGLISH+SUBLANG_DEFAULT, 0, \
+    'FileDescription',  'Biometric hotkeys', \
+    'FileVersion',      '0.0.0.0', \
+    'ProductName',      'HotFinger', \
+    'ProductVersion',   '0.0.0', \
+    'LegalCopyright',   'UNLICENSE', \
+    'OriginalFilename', 'hotfinger.exe'
